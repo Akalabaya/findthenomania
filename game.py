c0b93a51e6756c1c81a8a9304fbab7b23e5f005f
AKALABAYA PAL <65589371+Akalabaya@users.noreply.github.com>
2022-08-15 18:23:22 +0530

    Added multiplayer feature

--- game.py
+++ game.py
@@ -3,87 +3,112 @@ import os
 import sys
 from playsound import playsound
 import threading
+import colorama
+from colorama import Back,Fore
 
-def playsoundbackr():
-    while True:
-        playsound("background.mp3")
-
-thread = threading.Thread(target=playsoundbackr)
-
-thread.start()
+colorama.init()
+print(Back.YELLOW)
+print(Fore.RED)
+print(colorama.ansi.clear_screen())
 
 
-lt = 999
+playsound2 = True
 
-try:
+if len(sys.argv) >= 2:
     if  "/reset" in sys.argv[0] or "/reset" in  sys.argv[1]:
         if input("Are you sure you want to reset the score?") == "yes":
             os.remove("highscore.txt")
             print("Game high score reset completed...")
             exit(0)
-except:
-    pass
+    if  "/nosound" in sys.argv[1] or "/nosound" in  sys.argv[2]:
+        playsound2 = False
 
-print("================================================")
-print("Welcome to guessing mania....!!!")
-try:
-    print("Your Best:"+open("highscore.txt","r").read()+" Attempts...")    
-except:
-    print("Welcome for first time.....")
-print("================================================")
-print("")
 
-print("""
-1)Very Easy \n
-2)Easy \n
-3)Medium \n
-4)Hard \n
-5)Very Hard
-""")
-level = int(input("Select a level(Enter th level no. shown above):"))
-playsound("click.wav")
-if level == 1:
-    lt = 10
-elif level == 2:
-    lt = 100
-elif level == 3:
-    lt = 500
-elif level == 4:
-    lt == 999
-elif level == 5:
-    lt = 1500
-#print(n)
+def playsoundbackr():
+    if playsound2 == True:
+         while True:
+            playsound("background.mp3")
 
-n = random.randint(0,lt)
+thread = threading.Thread(target=playsoundbackr)
 
-gussed = 0
-attepmts = 0
-while gussed != n:
-    a = input("Guess the no:")
-    playsound("click.wav")
-    gussed = int(a)
-    if gussed > n:
-        print("Your guess is more than the real no.")
-    elif gussed < n:
-        print("Your guess is less than the real no.")
-    elif gussed > (lt+1):
-        print("Your guess is more than no. limit the limit is "+str(lt+1))
+thread.start()
 
-    print("")
-    attepmts += 1
+def play():
 
-print("")
-print("Yayy you got it!!")
-print("It took {} attempts to finish the game.".format(attepmts))
-playsound("success.mp3")
+	print("")
 
-if os.path.exists("highscore.txt"):
-    hs = int(open("highscore.txt","r").read())
-    if attepmts <= hs:
-        open("highscore.txt","w").write(str(attepmts))
-        print("Your new high score: {}".format(attepmts))
-else:
-     open("highscore.txt","w").write(str(attepmts))
-     print("Your new high score: {}".format(attepmts))
-input()
-exit()
+	print("================================================")
+	print("Welcome to guessing mania....!!!")
+	try:
+		print("Your Best:"+open("highscore.txt","r").read()+" Attempts...") 
+	except:
+		print("Welcome for the first time!!!")   	
+	print("================================================")
+	lt = 999
+	print(Fore.GREEN)
+	print("""
+1)Very Easy \n"""+Fore.CYAN+"""
+2)Easy \n"""+Fore.BLACK+"""
+3)Medium \n"""+Fore.LIGHTMAGENTA_EX+"""
+4)Hard \n"""+Fore.LIGHTRED_EX+"""
+5)Very Hard"""+Fore.RED+"""
+	""")
+	level = int(input("Select a level(Enter the level no. shown above):"))
+	playsound("click.wav")
+	if level == 1:
+		lt = 10
+	elif level == 2:
+		lt = 100
+	elif level == 3:lt = 500
+	elif level == 4:
+		lt == 999
+	elif level == 5:lt = 1500
+	#print(n)
+
+	n = random.randint(0,lt)
+	print(Fore.BLUE)
+	guessed = 0
+	attepmts = 0
+
+	while guessed != n:
+		a = input("Guess the no:")
+		playsound("click.wav")
+		guessed = int(a)
+		if guessed > n:print("Your guess is more than the real no.")
+		elif guessed < n:
+			print("Your guess is less than the real no.")
+		elif guessed > (lt+1):print("Your guess is more than no. limit the limit is "+str(lt+1))
+		print("")
+		attepmts += 1
+
+	print("")
+	print(Back.WHITE)
+	print(Fore.BLACK)
+	print("Yayy you got it!!")
+	print("It took {} attempts to finish the game.".format(attepmts))
+	playsound("success.mp3")
+
+	if os.path.exists("highscore.txt"): 
+		hs = int(open("highscore.txt","r").read())
+		if attepmts <= hs:
+			print("Your new high score: {}".format(attepmts))
+			open("highscore.txt","w").write(str(attepmts))
+	
+	else:
+		open("highscore.txt","w").write(str(attepmts))
+	
+
+
+	if input("Play another time?") == "yes":
+		colorama.init()
+		print(Back.YELLOW)
+		print(Fore.RED)
+		print(colorama.ansi.clear_screen())
+		play()
+	else:
+		print("Bye Byye!!!")
+
+
+
+
+play()
