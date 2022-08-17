a738d99904119b84904488fa8e0c007914474307
AKALABAYA PAL <65589371+Akalabaya@users.noreply.github.com>
2022-08-17 19:16:37 +0530

    Update

--- socketmodes/game_client_module.py
+++ socketmodes/game_client_module.py
@@ -63,7 +63,8 @@ def load_play():
 	sn = pyperclip.paste()
 	url = ""
 	if str(sn).startswith("https://") or str(sn).startswith("http://"):
-		url = str(sn)
+		if "ngrok" in str(sn):
+			url = str(sn)
 	else:
 		url = input("Enter a URL to connect:")
 	# Define the port on which you want to connect
